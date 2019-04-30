33492ecb20ec8a2db722c0f3e11ad55893ef46cb
Andrew Silva <andrewsilva9@gmail.com>
2019-04-30 11:14:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,6 +9,5 @@ def happy_birthday(birthday_kids)
     if age <= 12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-  happy_birthday
 end
   # add your code snippet here!
